5d7369e34ea77c51d08863fa7ea7c569d2257772
Thenukad Dinu <thenukadinu@gmail.com>
2021-02-23 15:07:30 +0530

    changes

--- todo-api/Controllers/TodoesController.cs
+++ todo-api/Controllers/TodoesController.cs
@@ -41,7 +41,7 @@ namespace todo_api.Controllers
             return todo;
         }
 
-        // PUT: api/Todoes/5
+        // PUT: api/Todoes/5 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
